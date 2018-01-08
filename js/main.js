4be4c6d771cf97ccc0fb49e073f85fe246b17a4e
mgb24 <mario.garita@gmail.com>
2018-01-08 09:32:29 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('body').on('mouseenter mouseleave','.dropdown',function(e){
+	  var _d=$(e.target).closest('.dropdown');_d.addClass('show');
+	  setTimeout(function(){
+		 _d[_d.is(':hover')?'addClass':'removeClass']('show');
+	  },300);
+	});
+});
